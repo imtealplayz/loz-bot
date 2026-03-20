2a233155b3a4cd9fbbeaf1d2835e04f0d340db29
Teal <tealplayzmc001@gmail.com>
2026-03-20 11:48:36 +0530

    Update state.js

--- state.js
+++ state.js
@@ -2,12 +2,14 @@ const { Collection } = require("discord.js");
 
 // ==================== SHARED STATE ====================
 const cooldowns       = new Collection();
+const challenges      = new Map();
 const leaderboard     = new Collection();
 const fightLeaderboard= new Collection();
 const fightStats      = new Collection();
 const duelCooldowns   = new Collection();
 const userSpecies     = new Collection();
 const botStats        = new Collection();
+const activeRolls     = new Map();
 const activeFights    = new Map();
 const fightChallenges = new Map();
 const fightCooldowns  = new Collection();
@@ -16,10 +18,11 @@ const activeBotFights = new Map();
 const fightMessages   = new Map();
 const questProgress   = new Collection();
 const activeRequests  = new Map();
+const duelChannels    = new Collection();
 
 module.exports = {
-  cooldowns, leaderboard, fightLeaderboard, fightStats,
-  duelCooldowns, userSpecies, botStats, activeFights, fightChallenges,
-  fightCooldowns, dailyClaims, activeBotFights, fightMessages,
-  questProgress, activeRequests,
+  cooldowns, challenges, leaderboard, fightLeaderboard, fightStats,
+  duelCooldowns, userSpecies, botStats, activeRolls, activeFights,
+  fightChallenges, fightCooldowns, dailyClaims, activeBotFights,
+  fightMessages, questProgress, activeRequests, duelChannels,
 };
